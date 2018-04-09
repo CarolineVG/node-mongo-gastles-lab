7d89c90cb3b26b53ed1ab68e5be9a1ead5551ae4
Caroline VG <caroline.vangossum@live.be>
2018-04-09 19:58:07 +0200

    update routes/student.js

--- routes/student.js
+++ routes/student.js
@@ -1,13 +1,19 @@
-const middleware = require('../middleware/auth');
+//const middleware = require('../middleware/auth');
+
+const studentsService = require('../services/students');
+
 
 module.exports = (app) => {
+    /* GET api students */
+    app.route('/api/students').get(studentsController.fetchAll);
+
     /* GET api/students */
-    app.route('/api/students').get((req, res) => {
+    /*app.route('/api/students').get((req, res) => {
         res.status(200).json(students); // 200: success -> show students.json
     });
 
     /* GET api/students id -> http://localhost:3000/api/students/1 */
-    app.route('/api/students/:id').get((req, res) => {
+    /*app.route('/api/students/:id').get((req, res) => {
         // search in students 
         const student = students.find((stu) => {
             // if student id == request id
@@ -28,21 +34,21 @@ module.exports = (app) => {
     /* MIDDLEWARE */
 
     /* POST api/students */
-    app.route('/api/students').post(middleware, (req, res) => {
+    /*app.route('/api/students').post(middleware, (req, res) => {
         // 201: created
         res.status(201).json(Object.assign({}, req.body, {
             id: (students.length + 1).toString(),
         }));
     });
     /* PUT api/students id */
-    app.route('/api/students/:id').put((req, res) => {
+    /*app.route('/api/students/:id').put((req, res) => {
         // 200: success
         res.status(200).json(req.body);
     });
 
     /* DELETE api/students id */
-    app.route('/api/students/:id').delete((req, res) => {
+    /*app.route('/api/students/:id').delete((req, res) => {
         // 204: no content -> delete 
         res.status(204).send();
-    });
+    });*/
 }
