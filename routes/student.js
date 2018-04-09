1a6d5ead4c1686c6ab6425138dacfdb299ea462d
Caroline VG <caroline.vangossum@live.be>
2018-04-09 16:55:58 +0200

    add middleware (POST, PUT, DELETE) to student.js

--- routes/student.js
+++ routes/student.js
@@ -17,6 +17,7 @@ module.exports = (app) => {
     app.route('/api/students').get((req, res) => {
         res.status(200).json(students); // 200: success -> show students.json
     });
+
     /* GET api/students id -> http://localhost:3000/api/students/1 */
     app.route('/api/students/:id').get((req, res) => {
         // search in students 
@@ -34,7 +35,26 @@ module.exports = (app) => {
                 message: 'Not Found'
             });
         }
+    });
 
-        
+    /* MIDDLEWARE */
+
+    /* POST api/students */
+    app.route('/api/students').post(middleware, (req, res) => {
+        // 201: created
+        res.status(201).json(Object.assign({}, req.body, {
+            id: (students.length + 1).toString(),
+        }));
+    });
+    /* PUT api/students id */
+    app.route('/api/students/:id').put((req, res) => {
+        // 200: success
+        res.status(200).json(req.body);
+    });
+
+    /* DELETE api/students id */
+    app.route('/api/students/:id').delete((req, res) => {
+        // 204: no content -> delete 
+        res.status(204).send();
     });
 }
