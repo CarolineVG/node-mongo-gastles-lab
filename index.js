12b57a5e910aa9a3f6cd41526ac73c38912bfd0c
Caroline VG <caroline.vangossum@live.be>
2018-04-09 16:04:25 +0200

    create hello-world.txt, read sync and async

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-console.log('Start Script!');
+/*console.log('Start Script!');
 
 setTimeout(() => {
     console.log('Hello world!');
 }, 1000);
 
-console.log('Stop Script!');
+console.log('Stop Script!');*/
+
+// require fs (file reader system)
+const fs = require('fs');
+
+// sync demo
+function readFileSyncDemo(path) {
+    console.log('START READING FILE SYNC:');
+
+    const data = fs.readFileSync(path, 'utf8');
+    console.log(data);
+
+    console.log('STOP READING FILE SYNC');
+}
+
+// async demo
+function readFileAsyncDemo(path) {
+    console.log('START READING FILE ASYNC:');
+
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) throw err; // if error toon error
+        console.log('DATA: ', data); // anders toon data
+    });
+
+    console.log('STOP READING FILE ASYNC');
+} 
+
+const demoPath = './hello-world.txt';
+readFileSyncDemo(demoPath);
+console.log('----------------------------------');
+readFileAsyncDemo(demoPath);
