42bd9285953fbc773046bfd16354876fac85f2f2
Caroline VG <caroline.vangossum@live.be>
2018-04-09 15:54:43 +0200

    explain promise-timeout.js

--- promise-timeout.js
+++ promise-timeout.js
@@ -1,7 +1,9 @@
 console.log('Start Script!');
 // object dat je maakt, doet iets, weet niet wnr het klaar is, vertelt wnr het klaar is en wnr het faalt
 const myPromise = () => {
+    // "promise to do something"
 	return new Promise(resolve => {
+        // resolve: als hello world geprint wordt
 		setTimeout(() => {
 			resolve("Hello world!");
 		}, 1000);
@@ -9,7 +11,7 @@ const myPromise = () => {
 };
 // functie wordt opgeroepen
 myPromise()
-    // wnr functie klaar is
+    // wnr functie klaar is (wacht op response)
 	.then((response) => {
 		console.log(response);
 	});
