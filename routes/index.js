2d6afbbcd1aa7d08478eb394042e888b1f74a066
Caroline VG <caroline.vangossum@live.be>
2018-04-09 16:17:59 +0200

    create routes/index.js file

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const glob = require("glob"); // requires multiple modules using glob patterns 
+const path = require("path"); // file and directory paths
+
+module.exports = (app) => {
+    // get home page and send hello world
+    app.get('/', (req, res) => {
+        res.send('Hello World')
+    });
+    
+    // ?
+    glob.sync("./routes/!(index).js", {
+        absolute: true,
+    }).forEach(route => {
+        require(route)(app);
+    });
+};
