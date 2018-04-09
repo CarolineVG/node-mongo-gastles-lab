b3a32e166e9bfd70921ff47c281c4de09f2f9851
Caroline VG <caroline.vangossum@live.be>
2018-04-09 15:42:31 +0200

    explain callback

--- callback-timeout.js
+++ callback-timeout.js
@@ -4,4 +4,9 @@ setTimeout(() => {
     console.log('Hello world!');
 }, 1000);
 
+// hetzelfde als
+/*setTimeout(function(){
+    console.log('Hello world!');
+},1000);*/
+
 console.log('Stop Script!');
