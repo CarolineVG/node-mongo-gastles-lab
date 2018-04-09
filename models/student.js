ec5166ebf0edb23f0f3be6421af4376613e0290a
Caroline VG <caroline.vangossum@live.be>
2018-04-09 20:21:57 +0200

    install mongoose and edit schema

---
+++ models/student.js
@@ -0,0 +1,14 @@
+// require mongoose
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const studentSchema = new Schema({ 
+    name: {
+        required: true,
+        type: String
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Student', studentSchema);
