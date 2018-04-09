3abeca8734e039525bf28c9fdd306c75247fc6f0
Caroline VG <caroline.vangossum@live.be>
2018-04-09 16:06:59 +0200

    Chapter 3: install express and update index.js

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
-/*console.log('Start Script!');
+const express = require('express');
+const app = express();
 
-setTimeout(() => {
-    console.log('Hello world!');
-}, 1000);
+const config = {
+    port: 3000,
+};
+ 
+app.get('/', (req, res) => {
+    // stuur hello world als er naar / gesurft wordt
+  res.send('Hello World')
+});
 
-console.log('Stop Script!');*/
-
-// require fs (file reader system)
-const fs = require('fs');
-
-// sync demo
-function readFileSyncDemo(path) {
-    console.log('START READING FILE SYNC:');
-
-    const data = fs.readFileSync(path, 'utf8');
-    console.log(data);
-
-    console.log('STOP READING FILE SYNC');
-}
-
-// async demo
-function readFileAsyncDemo(path) {
-    console.log('START READING FILE ASYNC:');
-
-    fs.readFile(path, 'utf8', (err, data) => {
-        if (err) throw err; // if error toon error
-        console.log('DATA: ', data); // anders toon data
-    });
-
-    console.log('STOP READING FILE ASYNC');
-} 
-
-const demoPath = './hello-world.txt';
-readFileSyncDemo(demoPath);
-console.log('----------------------------------');
-readFileAsyncDemo(demoPath);
+// luister naar de poort (3000)
+app.listen(config.port, () => {
+    console.log(`Server listening at port ${config.port}.`);
+});
