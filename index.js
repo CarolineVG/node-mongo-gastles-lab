7927610ed04da2325b9e5eb2a515d4b925266cbe
Caroline VG <caroline.vangossum@live.be>
2018-04-09 16:23:48 +0200

    edit index.js file - remove previous route

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ const config = {
 
 // Init routes
 require("./routes/")(app);
- 
-app.get('/', (req, res) => {
-    // stuur hello world als er naar / gesurft wordt
-  res.send('Hello World')
-});
 
 // luister naar de poort (3000)
 app.listen(config.port, () => {
