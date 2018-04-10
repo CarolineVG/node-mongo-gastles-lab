afd03ab26f89d5f74d817f14b5989048fda1da87
Caroline VG <caroline.vangossum@live.be>
2018-04-10 14:57:58 +0200

    edit index.js file

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const config = {
     port: 3000,
 };
+
+// mongo db 
+mongoose.connect('mongodb://localhost/imd-demo-app', (err) => {
+    if (err) {
+        console.log(err);
+    }
+});
 
 // Body parser
 app.use(bodyParser.json({ limit: "50mb", keepExtensions: true }));
