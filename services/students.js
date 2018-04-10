bc0b89b78f1b3fc598e85a02ad8357aaa95cd398
Caroline VG <caroline.vangossum@live.be>
2018-04-10 14:36:53 +0200

    update services/student.js create - update - delete

--- services/students.js
+++ services/students.js
@@ -28,3 +28,30 @@ module.exports.fetchById = (id) => {
     // .lean -> return js objects (niet mongoose)
     return StudentModel.findOne({ _id: id }).lean().exec();
 }
+
+// create
+StudentModel.create(student);
+
+// update
+module.exports.update = (id, student) => {
+    // als id niet bestaat is return null
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return new Promise((resolve, reject) => {
+            resolve(null);
+        });
+    }
+    // anders zoek id en update
+    return StudentModel.findOneAndUpdate({ _id: id }, student, { new: true });
+}
+
+// delete 
+module.exports.delete = (id) => {
+    // als id niet bestaat return null
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return new Promise((resolve, reject) => {
+            resolve(null);
+        });
+    }
+    // anders zoek id en remove 
+    return StudentModel.remove({ _id: id }).exec();
+}
